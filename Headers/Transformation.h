96dffa48ce160648b35c1cba821c0716e9b40fe3
Nathan Hoad <nathan@getoffmalawn.com>
2011-04-03 11:42:02 +1000

    Vector and Matrix multiplication complete.

--- Headers/Transformation.h
+++ Headers/Transformation.h
@@ -15,8 +15,8 @@ class Mat4
 		Mat4& operator=(const Mat4& m);
 		friend std::ostream & operator<<(std::ostream & o, const Mat4 & m);
 
-		Mat4 operator*(const Mat4 & m);
-		Vec4 operator*(const Vec4 & m);
+		static Mat4 mul(const Mat4& m, const Mat4& n);
+		static Vec4 mul(const Mat4& m, const Vec4& v);
 };
 
 class Vec4
@@ -24,5 +24,10 @@ class Vec4
 	public:
 		float x, y, z, w;
 		Vec4(float x=0, float y=0, float z=0, float w=1) : x(x), y(y), z(z), w(w) { };
+
+		float& operator()(int x);
+		float operator()(int x) const;
+
+		friend std::ostream & operator<<(std::ostream & o, const Mat4 & m);
 
 };
--- Source/Transformation.cpp
+++ Source/Transformation.cpp
@@ -22,6 +22,40 @@ Mat4::Mat4(float * values)
 		data[i] = values[i];
 }
 
+float Vec4::operator()(int x) const
+{
+	if (x == 0)
+		return this->x;
+	else if (x == 1)
+		return this->y;
+	else if (x == 2)
+		return this->z;
+	else
+		return this->w;
+}
+
+float& Vec4::operator()(int x)
+{
+	if (x == 0)
+		return this->x;
+	else if (x == 1)
+		return this->y;
+	else if (x == 2)
+		return this->z;
+	else
+		return this->w;
+}
+
+ostream & operator<<(ostream & o, const Vec4 & v)
+{
+	o << "x: " << v.x << endl;
+	o << "y: " << v.y << endl;
+	o << "z: " << v.z << endl;
+	o << "w: " << v.w << endl;
+
+	return o;
+}
+
 float Mat4::operator()(int x, int y) const
 {
 	return data[(x * 4) + y];
@@ -41,16 +75,25 @@ Mat4& Mat4::operator=(const Mat4& m)
 
 }
 
-Mat4 Mat4::operator*(const Mat4 & m)
+Mat4 Mat4::mul(const Mat4& m, const Mat4& n)
 {
 	Mat4 result(0.0);
-
-	int row = 0;
 	for (int row=0; row < 4; row++)
 		for (int column=0; column < 4; column++)
-		{
 			for (int i=0; i < 4; i++)
-				result(row, column) += (*this)(row, i) * m(i, column);
+				result(row, column) += m(row, i) * n(i, column);
+	return result;
+}
+
+Vec4 Mat4::mul(const Mat4& m, const Vec4& v)
+{
+	Vec4 result(0, 0, 0, 0);
+
+	for (int row=0; row < 4; row++)
+		for (int i=0; i < 4; i++)
+		{
+			cout << "multiplying " << m(row, i) << " by " << v(i) << endl;
+			result(row) += m(row, i) * v(i);
 		}
 
 	return result;
@@ -66,17 +109,11 @@ ostream & operator<<(ostream & o, const Mat4 & m)
 
 int main(void)
 {
-	Mat4 m, n;
+	Mat4 m;
 
-	m(0, 1) = 5;
-	n(0, 1) = 9;
-	cout << m << endl;
-	cout << n << endl;
+	Vec4 a(4, 15, 3);
 
-	Mat4 r = m * n;
-
-	cout << r << endl;
-
+	cout << Mat4::mul(m, a) << endl;
 }
 
 
